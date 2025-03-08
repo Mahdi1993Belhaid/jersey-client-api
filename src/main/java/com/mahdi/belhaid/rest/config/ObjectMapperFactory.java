92673bfa357fe63fd9a433f007391fa9f1f80bdb
belhaid mahdi <dzbelhaidmahdi@gmail.com>
2025-03-08 13:06:21 +0100

    fix thread safety in ObjectMapper factory class

--- src/main/java/com/mahdi/belhaid/rest/config/ObjectMapperFactory.java
+++ src/main/java/com/mahdi/belhaid/rest/config/ObjectMapperFactory.java
@@ -5,13 +5,17 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class ObjectMapperFactory {
 
     private ObjectMapperFactory(){}
-    private static ObjectMapper instance = null;
+    private static volatile ObjectMapper instance = null;
 
     public static ObjectMapper getInstance() {
         if (instance == null) {
-            instance = new ObjectMapper();
+            synchronized (ObjectMapperFactory.class){
+                if(instance == null){
+                    instance = new ObjectMapper();
+                }
+            }
         }
-        return new ObjectMapper();
+        return instance;
     }
 
 }
